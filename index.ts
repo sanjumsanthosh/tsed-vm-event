96d809487ad27300f52d877df7a94afd37820b68
sanjay <44315700+sanjumsanthosh@users.noreply.github.com>
2024-06-01 16:30:33 +0530

    chore: Add lazy loading for images and update npm dependencies

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { Database } from "bun:sqlite";
-
+import {ulid} from "ulid";
 
 
 const server = Bun.serve({
@@ -30,14 +30,128 @@ const server = Bun.serve({
             saveToSQLite(data.url, data.data);
             return new Response("Received!");
         }
+        if (url.pathname === "/db" && req.method === "GET") {
+            const pageNumber = parseInt(url.searchParams.get("pageNumber") || "0", 10);
+            const pageCount = parseInt(url.searchParams.get("pageCount") || "10", 10);
+            console.log(`pageNumber: ${pageNumber}`);
+            console.log(`pageCount: ${pageCount}`);
+            const rows = getRowsFromSQLite(pageNumber, pageCount);
+            return new Response(JSON.stringify(rows), {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+        }
+        if (url.pathname === "/db/mark" && req.method === "POST") {
+            const id = url.searchParams.get("id");
+            console.log(`Marking as read: ${id}`);
+            if (!id) {
+                return new Response("Please provide an id!", { status: 400 });
+            }
+            markReadInSQLite(id);
+            return new Response("Marked as read!");
+        }
+        if (url.pathname === "/db/clean" && req.method === "DELETE") {
+            console.log(`Removing read rows`);
+            removeMarkedReadFromSQLite();
+            return new Response("Removed read rows!");
+        }
+        if (url.pathname === "/db/stats" && req.method === "GET") {
+            console.log(`Getting stats`);
+            const stats = getDBStats();
+            return new Response(JSON.stringify(stats), {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+        }
         return new Response("404!");
       },
+
 });
 
 function saveToSQLite(url: string, data: string) {
-    const db = new Database(":memory:");
-    const query = db.query("select 'Hello world' as message;");
-    console.log(query.get());
+    const db = new Database("mydb.sqlite", { create: true });
+    const formatedUrl = new URL(url).origin + new URL(url).pathname;
+    const createTableQuery = db.query(`
+        CREATE TABLE IF NOT EXISTS content (
+        id TEXT PRIMARY KEY,
+        url TEXT UNIQUE,
+        data BLOB,
+        date TEXT,
+        read INTEGER
+        );
+    `);
+    createTableQuery.run();
+
+    // Check if URL exists
+    const urlExistsQuery = db.query(`SELECT url FROM content WHERE url = ?`);
+    const urlExists = urlExistsQuery.get(formatedUrl);
+
+    if (urlExists) {
+        // If URL exists, update the row
+        const updateDataQuery = db.query(`
+            UPDATE content
+            SET data = ?, date = ?, read = ?
+            WHERE url = ?
+        `);
+        updateDataQuery.run(data, new Date().toISOString(), 0, formatedUrl);
+    } else {
+        // If URL does not exist, insert a new row
+        const insertDataQuery = db.query(`
+            INSERT INTO content (id, url, data, date, read)
+            VALUES (?, ?, ?, ?, ?)
+        `);
+        insertDataQuery.run(ulid(), url, data, new Date().toISOString(), 0);
+    }
+
+    // print count of rows
+    const countQuery = db.query(`SELECT COUNT(*) as count FROM content`);
+    console.log(`Rows in the table: ${JSON.stringify(countQuery.get())}`);
+}
+
+function getRowsFromSQLite(pageNumber: number, pageCount: number) {
+    const db = new Database("mydb.sqlite", { create: true });
+    const selectQuery = db.query(`
+        SELECT * FROM content
+        ORDER BY date DESC
+        LIMIT ${pageCount} OFFSET ${pageNumber * pageCount}
+    `);
+    const rows = selectQuery.all();
+    return rows;
+}
+
+function markReadInSQLite(id: string) {
+    const db = new Database("mydb.sqlite", { create: true });
+    const markReadQuery = db.query(`
+        UPDATE content
+        SET read = 1
+        WHERE id = ?
+    `);
+    markReadQuery.run(id);
+}
+
+function getDBStats() {
+    const db = new Database("mydb.sqlite", { create: true });
+    const countQuery = db.query(`SELECT COUNT(*) as count FROM content`);
+    const readCountQuery = db.query(`SELECT COUNT(*) as count FROM content WHERE read = 1`);
+    const unreadCountQuery = db.query(`SELECT COUNT(*) as count FROM content WHERE read = 0`);
+    const sizeQuery = db.query(`SELECT page_count * page_size as size FROM pragma_page_count(), pragma_page_size()`);
+    const rows = countQuery.get();
+    const readRows = readCountQuery.get();
+    const unreadRows = unreadCountQuery.get();
+    const size = sizeQuery.get() as {size: string};
+    const sizeInMB = `${parseFloat(size.size) / 1024 / 1024} MB`;
+    return {rows, readRows, unreadRows, sizeInMB};
+}
+
+function removeMarkedReadFromSQLite() {
+    const db = new Database("mydb.sqlite", { create: true });
+    const removeQuery = db.query(`
+        DELETE FROM content
+        WHERE read = 1
+    `);
+    removeQuery.run();
 }
 
 function trucateLogger(data: string){
