b9ca0054b87b54c36bced83c939e2c57c8d2f5e0
sanjay <44315700+sanjumsanthosh@users.noreply.github.com>
2024-06-01 16:06:13 +0530

    feat: Add error handling for parsing webhook body

--- index.ts
+++ index.ts
@@ -1,21 +1,47 @@
+import { Database } from "bun:sqlite";
+
+
+
 const server = Bun.serve({
     port: 8083,
     async fetch(req) {
         const url = new URL(req.url);
-        // if (url.pathname === "/") return new Response("Home page!");
-        // if (url.pathname === "/blog") return new Response("Blog!");
-        // http://140.245.24.43:8083/rest/webhook
         if (url.pathname === "/rest/webhook" && req.method === "POST") {
-            // read body as plain text
             const body = await req.text();
             console.log(`Received request`);
-            const data = JSON.parse(body);
+            let data;
+            try {
+                data = JSON.parse(body);
+            } catch (e) {
+                const regex = /"url"\s*:\s*"([^"]+)"\s*,\s*"data"\s*:\s*"([\s\S]+)"/m;
+                const match = body.match(regex);
+                if (match) {
+                        data = {
+                        url: match[1],
+                        data: match[2]
+                        };
+                    } else {
+                        console.error("Could not parse body");
+                        return new Response("Could not parse body", { status: 400 });
+                    }
+            }
             console.log(`URL: ${data.url}`);
-            console.log(`Data: ${data.data}`);
-            return new Response("Webhook!");
+            console.log(`Data: ${trucateLogger(data.data)}`);
+            saveToSQLite(data.url, data.data);
+            return new Response("Received!");
         }
         return new Response("404!");
       },
-  });
+});
+
+function saveToSQLite(url: string, data: string) {
+    const db = new Database(":memory:");
+    const query = db.query("select 'Hello world' as message;");
+    console.log(query.get());
+}
+
+function trucateLogger(data: string){
+    return data.length > 50 ? `${data.substring(0, 50)}...` : data;
+}
   
-  console.log(`Listening on http://localhost:${server.port} ...`);
+console.log(`Listening on http://localhost:${server.port} ...`);
