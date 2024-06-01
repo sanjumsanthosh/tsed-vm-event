d9984dc52aaf2787d244feeda5b0e9ce58a36e2e
sanjay <44315700+sanjumsanthosh@users.noreply.github.com>
2024-06-01 15:41:48 +0530

    chore: Initialize Bun server and add webhook endpoint

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+    port: 8083,
+    async fetch(req) {
+        const url = new URL(req.url);
+        // if (url.pathname === "/") return new Response("Home page!");
+        // if (url.pathname === "/blog") return new Response("Blog!");
+        // http://140.245.24.43:8083/rest/webhook
+        if (url.pathname === "/rest/webhook" && req.method === "POST") {
+            // read body as plain text
+            const body = await req.text();
+            console.log(body);
+            return new Response("Webhook!");
+        }
+        return new Response("404!");
+      },
+  });
+  
+  console.log(`Listening on http://localhost:${server.port} ...`);
